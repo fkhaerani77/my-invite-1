59740919fd7258c14113fe849a9da242a7898fcf
Fauzia Khaerani <fkhaerani77@gmail.com>
2025-08-05 10:10:22 +0700

    naikin-speed-saat-scroll

--- src/App.js
+++ src/App.js
@@ -35,14 +35,14 @@ function App() {
   useEffect(() => {
     if (!isOpened || !isAutoScroll) return;
 
-    let scrollSpeed = 1;
+    let scrollSpeed = 3;
     let interval;
 
     const scrollDown = () => {
       window.scrollBy({ top: scrollSpeed, behavior: "smooth" });
     };
 
-    interval = setInterval(scrollDown, 5);
+    interval = setInterval(scrollDown, 3);
 
     const onUserScroll = (e) => {
       if (e.deltaY < 0) {

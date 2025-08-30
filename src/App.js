3fa0fc8445c8ad9fa1e66a5a25dcdae25b0cff0b
Fauzia Khaerani <fkhaerani77@gmail.com>
2025-08-30 23:38:54 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
         clearInterval(scrollInterval);
         setIsAutoScroll(false);
       } else {
-        window.scrollBy({ top: 8, behavior: "smooth" });
+        window.scrollBy({ top: 9, behavior: "smooth" });
       }
     };
 
